e6aca5f78748f8ff7789dd2dc5a1dd27460fd177
Phellipe <slipkgao@hotmail.com>
2018-07-13 18:16:28 -0300

    projeto

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'home', to:'home#index'
+  end
+  get 'admin', to: 'backoffice/dashboard#index'
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  
+  namespace :backoffice do
+    get 'dashboard', to: 'dashboard#index'
+  end
+  devise_for :admins
+  devise_for :members
+  root 'site/home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
