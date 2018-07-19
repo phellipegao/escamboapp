b909c37cfa915b35c1845f55267eb59b2559703a
Phellipe <slipkgao@hotmail.com>
2018-07-19 20:05:28 -0300

    create registre of administrator

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,4 +1,40 @@
 class Backoffice::AdminsController < BackofficeControle
+before_action :set_admin, only: [:edit, :update]
   def index
+  	@admins = Admin.all
   end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create 
+    @admin = Admin.new(params_admin)
+    if @admin.save 
+      redirect_to backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi cadastro com sucesso!"
+    else
+      render :new 
+    end
+  end
+
+  def edit      
+  end
+
+  def update
+    if @admin.update(params_admin)
+       redirect_to backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi atualizado com sucesso!"
+    else
+      render :edit 
+    end
+  end
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
 end
+
